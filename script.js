f033c85097523d1907096cadac9b7d924fe448bd
Ray <77224234+rayhan5497@users.noreply.github.com>
2025-07-24 18:20:54 +0600

    load unlimited movies onscroll, fixed styles

--- script.js
+++ script.js
@@ -1,26 +1,124 @@
-document.getElementById('searchBtn').addEventListener('click', searchTorrents);
+const searchInput = document.getElementById('searchInput');
 
-async function searchTorrents() {
-  const query = document.getElementById('searchInput').value;
+let pagecount = 1;
+
+const parameters = {
+  limit: ['50'],
+  page: pagecount,
+  sort_by: ['year', 'date_added', 'seeds', 'peers', 'rating', 'download_count', 'like_count'],
+  genre: ['adventure', 'action'],
+};
+
+document.getElementById('searchBtn').addEventListener('click', () => {
+  getMovies(sortMovies());
+});
+
+let isSearched = false;
+let isFirstLoad = false;
+document.addEventListener('DOMContentLoaded', () => {
+  isSearched = false;
+  getMovies(sortMovies());
+  isFirstLoad = true;
+});
+
+document.addEventListener('scroll', () => {
+  if (isSearched) return;
+
+  const scrollTop = window.scrollY || window.pageYOffset;
+  const clientHeight = document.documentElement.clientHeight;
+  const scrollHeight = document.documentElement.scrollHeight;
+
+  if (scrollTop + clientHeight >= scrollHeight + -5) {
+    onScrollToEnd();
+  }
+});
+
+
+let isLoading = false;
+let isScrolledToEnd = false;
+
+const onScrollToEnd = () => {
+  if (isLoading) return;
+  isLoading = true;
+  if (searchInput.value) {
+    searchInput.value = '';
+  }
+
+  getMovies(sortMovies()).finally(() => {
+    isLoading = false;
+    isScrolledToEnd = true;
+  });
+};
+
+const getDefaultParameter = () => {
+  return `limit=${parameters.limit}&sort_by=${parameters.sort_by[6]}&page=${parameters.page}`;
+};
+
+const sortMovies = () => {
+  const searchValue = searchInput.value;
+  const query = `query_term=${encodeURIComponent(searchValue)}`;
+  if (searchValue.length > 0) {
+    isSearched = true;
+    return query;
+  } else {
+    return getDefaultParameter();
+  }
+};
+
+async function getMovies(sortedMovies) {
+  const query = sortedMovies;
+
   if (!query) return;
 
+  const container = document.querySelector('.container');
   const resultsContainer = document.getElementById('results');
-  resultsContainer.innerHTML = '<p class="loading">Loading...</p>';
+  const resultItem = resultsContainer.querySelector('.result-item');
+  if (!resultItem) {
+    resultsContainer.innerHTML = '<p class="loading">Loading...</p>';
+  } else {
+    const showMore = document.createElement('h1');
+    showMore.classList.add('show-more');
+    showMore.textContent = 'Loading More Movies';
+    container.appendChild(showMore);
+    console.log(showMore);
+  }
 
   try {
     const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?query_term=${encodeURIComponent(
-        query
-      )}`
+      `https://yts.mx/api/v2/list_movies.json?${sortedMovies}`
     );
-    const data = await response.json();
 
-    resultsContainer.innerHTML = '';
+    if (isSearched) {
+      resultsContainer.innerHTML = '';
+    }
+
+    if (searchInput.value) {
+      searchInput.value = '';
+      pagecount = 1;
+    }
+
+    console.log(pagecount);
+    if (isScrolledToEnd || isFirstLoad) {
+      pagecount = pagecount + 1;
+    }
+    parameters.page = pagecount;
+
+    const data = await response.json();
     console.log('data', data);
 
-    if (data.data.movies.length === 0) {
-      resultsContainer.innerHTML = '<p class="loading">No torrents found.</p>';
-      return;
+    const loading = document.querySelector('.loading');
+    const showMore = document.querySelector('.show-more');
+
+    if (loading) {
+      resultsContainer.innerHTML = '';
+
+      if (data.data.movies.length === 0) {
+        resultsContainer.innerHTML =
+          '<p class="loading">No torrents found.</p>';
+        return;
+      }
+    } else if (showMore) {
+      showMore.remove();
     }
 
     let current = 0;
@@ -42,8 +140,20 @@ async function searchTorrents() {
         document.getElementById('bg2'),
       ];
 
+              const next = (current + 1) % 2;
+              const poster = movie.large_cover_image;
+
+      bgs[current].style.backgroundImage = `url(${poster})`;
+      if (!bgs[current].classList.contains('active')) {
+        bgs[next].classList.remove('active');
+        bgs[current].classList.add('active');
+      } else {
+        bgs[next].style.backgroundImage = `url(${poster})`;
+        bgs[next].classList.add('active');
+        bgs[current].classList.remove('active');
+        current = next;
+      }
       item.addEventListener('mouseenter', () => {
-        const poster = movie.large_cover_image;
         const next = (current + 1) % 2;
 
         bgs[next].style.backgroundImage = `url(${poster})`;
@@ -97,24 +207,22 @@ async function searchTorrents() {
             videoPlayer.src = `https://multiembed.mov/?video_id=${imdbId}`;
             playerContainer.style.display = 'block';
             container.style.filter = 'blur(20px)';
+            container.style.pointerEvents = 'none';
           }
-
         } catch (err) {
           console.error('Failed to fetch or play video:', err);
         }
       });
     });
-
+    
     const closePlayer = document
-      .querySelector('.close-player')
-      .addEventListener(
-        'click',
-        () => {
-          playerContainer.style.display = 'none';
-          container.style.filter = 'none';
-          videoPlayer.src= 'about:blank';
-        }
-      );
+    .querySelector('.close-player')
+    .addEventListener('click', () => {
+      playerContainer.style.display = 'none';
+      container.style.filter = 'none';
+      container.style.pointerEvents = 'all';
+        videoPlayer.src = 'about:blank';
+      });
 
     document.querySelectorAll('.more-info-btn').forEach((button) => {
       button.addEventListener('click', async (event) => {
@@ -141,9 +249,17 @@ async function searchTorrents() {
     document.getElementById('closeModal').addEventListener('click', () => {
       document.getElementById('movieModal').style.display = 'none';
     });
+    document.getElementById('backdrop').addEventListener('click', () => {
+      document.getElementById('movieModal').style.display = 'none';
+    });
   } catch (error) {
     console.error('Error fetching torrents:', error);
-    resultsContainer.innerHTML =
-      '<p class="loading">Error fetching torrents. Try again later.</p>';
+    const showMore = document.querySelector('.show-more');
+    if (!resultItem) {
+      resultsContainer.innerHTML =
+        '<p class="loading">Error fetching torrents. Try again later.</p>';
+    } else if (showMore) {
+      showMore.remove();
+    }
   }
 }
